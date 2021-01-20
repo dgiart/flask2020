d277d45f31d9e785ac36bd98170e3c5ddd2a0a89
zlfqDTwy <zlfqdtwy@email.com>
2021-01-20 18:27:54 +0200

    20.01.2021 first in 2021

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 from app import app
 from app import db
 
@@ -8,4 +9,6 @@ app.register_blueprint(posts, url_prefix = '/blog')
 import view
 
 if __name__ == '__main__':
+    import sys
+    print(sys.version)
     app.run()
