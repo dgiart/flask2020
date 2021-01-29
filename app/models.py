a77b79255d8021f855270aba5ad4eb8b8ab0617d
zlfqDTwy <zlfqdtwy@email.com>
2021-01-29 17:13:49 +0200

    29_01_2021, from work

--- app/models.py
+++ app/models.py
@@ -19,6 +19,8 @@ class Post(db.Model):
     slug=db.Column(db.String(140),unique=True)
     body=db.Column(db.Text)
     created=db.Column(db.DateTime, default=datetime.now())
+    tags=db.relationship('Tag',secondary=post_tags,backref=db.backref('posts',lazy='dynamic'))
+
 
     def __init__ (self, *args, **kwargs):
         super(Post,self). __init__(*args, **kwargs)
