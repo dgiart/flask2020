1bd8fdbdb9125311ec32968984a277bd4be434a6
zlfqDTwy <zlfqdtwy@email.com>
2021-02-05 17:17:44 +0200

    05.02.2021

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,14 +1,21 @@
 from flask import Blueprint
 from flask import render_template
-
+from flask import request
 from models import Post, Tag
+from app import db
 posts = Blueprint('posts', __name__, template_folder = 'templates')
 
 @posts.route('/')
 def index():
-    posts = Post.query.all()
-    # name1 = 'Artem'
-    # name2 = 'HtmlBlueprint'
+    q = request.args.get('Q')
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
+        if posts.count() == 0:
+            not_found='No results found'
+            p=Post(title=not_found)
+            posts = Post.query.filter(Post.title.contains(not_found))
+    else:
+        posts = Post.query.all()
     return render_template('posts/index.html', posts = posts)
 
 @posts.route('/<slug>')
--- app/view.py
+++ app/view.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import render_template
 @app.route('/')
+
+
+# def foo():
+#     return 0
 def index():
-    name1 = 'Art'
+    l=[1,2,3,4,5,6,7,8,9]
+    name1 = 'Artem'
     name2 = 'Html'
-    return render_template('index.html', n = name1, m = name2)
+    return render_template('index.html', n = name1, m = name2, list=l)
