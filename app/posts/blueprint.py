223845f92644d978258ab929b220e1adfe0f3e4c
zlfqDTwy <zlfqdtwy@email.com>
2021-02-08 20:45:43 +0200

    20.44 , 08/02/21 works with forms, create_post ....

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -3,17 +3,29 @@ from flask import render_template
 from flask import request
 from models import Post, Tag
 from app import db
+
+from . forms import PostForm
+
 posts = Blueprint('posts', __name__, template_folder = 'templates')
+
+
+
+@posts.route('/create')
+def create_post():
+    form=PostForm()
+    return render_template('posts/post_create.html', form=form)
+
 
 @posts.route('/')
 def index():
     q = request.args.get('Q')
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
-        if posts.count() == 0:
-            not_found='No results found'
-            p=Post(title=not_found)
-            posts = Post.query.filter(Post.title.contains(not_found))
+        # if posts.count() == 0:
+        #     not_found='No results found'
+        #     p=Post(title=not_found)
+        #
+        #     posts = Post.query.filter(Post.title.contains(not_found))
     else:
         posts = Post.query.all()
     return render_template('posts/index.html', posts = posts)
