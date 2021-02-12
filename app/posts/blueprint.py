13e3d234316478eb996d473a1f3980f2e34a0e05
zlfqDTwy <zlfqdtwy@email.com>
2021-02-12 17:17:44 +0200

    12.02.21 edit button added

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -30,28 +30,56 @@ def create_post():
             print('Smth went Wrong')
         return redirect(url_for('posts.index'))
     form=PostForm()
-    return render_template('posts/post_create.html', form=form)
+    return render_template('posts/post_create.html', form = form)
+
+
+@posts.route('/<slug>/edit', methods=['POST', 'GET'])
+def edit_post(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+    if request.method == 'POST':
+
+        form = PostForm(formdata = request.form, obj = post)
+        form.populate_obj(post)
+        db.session.commit()
+        return redirect(url_for('posts.post_detail', slug = slug) )
+    form = PostForm(obj = post)
+    return render_template('/posts/post_edit.html', form = form, post = post)
 
 
 @posts.route('/')
 def index():
     q = request.args.get('Q')
     page=request.args.get('page')
-    if page and page.isdigit():
-        page=int(page)
-    else:
-        page=1
+
 
     if q:
         posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
+        # num_posts = posts.count()
     else:
         posts = Post.query.order_by(Post.created.desc())
+
+    num_posts = posts.count()
+    # logger.warning('!!!!!number Of POSTS = ' + str(num_posts))
+    # logger.warning('!!!!!PAGE number = ' + str(page))
+
+    if page and page.isdigit():
+        page=int(page)
+        if page >= num_posts:
+            page = 1
+    else:
+        page = 1
+
+
 
     pages=posts.paginate(page=page, per_page=5)
     return render_template('posts/index.html', posts = posts, pages=pages)
 
 @posts.route('/<slug>')
 def post_detail(slug):
+    q = request.args.get('Q')
+    if q:
+        return redirect(url_for('posts.index'))
+
     post=Post.query.filter(Post.slug==slug).first()
     logger.warning('SLUG =' + slug)
     tags = post.tags
@@ -60,6 +88,9 @@ def post_detail(slug):
 
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
+    q = request.args.get('Q')
+    if q:
+        return redirect(url_for('posts.index'))
     tag = Tag.query.filter(Tag.slug == slug).first()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag = tag, posts = posts)
